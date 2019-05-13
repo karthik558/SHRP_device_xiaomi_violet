570e2d4f6b80e9afc9d1ec1f0f0ec7ada6d58784
bigbiff <bigbiff@teamw.in>
2019-05-13 22:46:49 +0000

    Revert "Update License"
    This reverts commit e5e497af1152f18d76cbea4e3b4eec0382e7780a.
    
    Reason for revert: We should keep original license copyrights for the BSD license.
    
    Change-Id: Ic81bb4d1ed5d4d9fdb6589b3ba0ea7a8d872e817

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2019 LineageOS-16.0
+# Copyright (C) 2019 The TwrpBuilder Open-Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
