669776b1b340a11aa23beaf720889254985c2025
Erfan Abdi <erfangplus@gmail.com>
2020-04-02 03:04:40 +0530

    violet: Add some TWRP configs
    Change-Id: I6fdd24b12a7be719c0f921f9f62bcbf2059117a0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -103,7 +103,6 @@ TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 
 # Crypto
 TW_INCLUDE_CRYPTO := true
-TW_INCLUDE_FBE := true
 
 # TWRP specific build flags
 TW_THEME := portrait_hdpi
@@ -113,11 +112,14 @@ TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_EXTRA_LANGUAGES := true
 TW_INCLUDE_NTFS_3G := true
+TW_INCLUDE_FBE := true
+TW_EXCLUDE_SUPERSU := true
 TW_DEFAULT_LANGUAGE := en
 TW_NO_LEGACY_PROPS := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_MAX_BRIGHTNESS := 4095
 TW_DEFAULT_BRIGHTNESS := 1950
+TW_USE_TOOLBOX := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
