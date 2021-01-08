385c9dad2e95a1b624bc2e081ba0b9f99e03d5a8
Brock5555 <77067332+Brock5555@users.noreply.github.com>
2021-01-08 22:27:04 +0530

    Adapt to shrp

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -151,3 +151,25 @@ TW_INCLUDE_FUSE_EXFAT := true
 TW_INCLUDE_FUSE_NTFS := true
 TARGET_OTA_ASSERT_DEVICE := violet
 TW_EXCLUDE_TWRPAPP := true
+
+# SHRPIFY
+
+SHRP_PATH := device/xiaomi/violet
+SHRP_MAINTAINER := Brock
+SHRP_DEVICE_CODE := violet
+SHRP_EDL_MODE := 1
+SHRP_EXTERNAL := /sdcard1
+SHRP_INTERNAL := /data
+SHRP_OTG := /usb_otg
+SHRP_FLASH := 1
+SHRP_FONP_1 := /sys/class/leds/led:torch_0/brightness
+SHRP_FONP_2 := /sys/class/leds/led:torch_1/brightness
+SHRP_FONP_3 := /sys/class/leds/led:switch/brightness
+SHRP_FLASH_MAX_BRIGHTNESS := 200
+SHRP_REC := /dev/block/bootdevice/by-name/recovery
+SHRP_REC_TYPE := Treble
+SHRP_DEVICE_TYPE := A_Only
+SHRP_STATUSBAR_RIGHT_PADDING := 40
+SHRP_STATUSBAR_LEFT_PADDING := 40
+SHRP_NOTCH := true
+SHRP_DARK := true
--- omni_violet.mk
+++ omni_violet.mk
@@ -37,3 +37,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
     ro.build.date \
     ro.build.date.utc
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    fbe.data.wrappedkey=true
